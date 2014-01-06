f4a0d3039d8f177a58a1e8e149dcc64582d3b2bc
Andrey Krahmal <coolrighthere@yahoo.com>
2014-01-06 17:35:00 +0000

    Further refactoring. Got rid of student_number variable. Now using arrays to store student names.

--- directory.rb
+++ directory.rb
@@ -1,30 +1,36 @@
 #!/usr/bin/env ruby
 
-student_count = 16
+
+
+#create an array of students
+
+students = [
+'Andrey',
+ 'James',
+ 'Peter',
+ 'Berta',
+ 'Megan',
+ 'Jorja',
+ 'Nabin',
+ 'Biwek',
+ 'Colin',
+ 'Abraham',
+ 'Ayaz',
+ 'Rolando',
+ 'Kalle',
+ 'Roy',
+ 'Liviu',
+ 'Makis'
+]
 
 # print a list of students
 puts '___________________________________________'
 puts 'The students of my cohort at Makers Academy'
 puts '___________________________________________'
-puts 'Andrey'
-puts 'James'
-puts 'Peter'
-puts 'Berta'
-puts 'Megan'
-puts 'Jorja'
-puts 'Nabin'
-puts 'Biwek'
-puts 'Colin'
-puts 'Abraham'
-puts 'Ayaz'
-puts 'Rolando'
-puts 'Kalle'
-puts 'Roy'
-puts 'Liviu'
-puts 'Makis'
+student.each {|name| puts name} 
 puts '___________________________________________'
 
 # print the total 
-puts "Overall we have #{student_count} great students"
+puts "Overall we have #{student.length} great students"
 puts '___________________________________________'
 

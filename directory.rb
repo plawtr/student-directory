22ee98da9af13c79079ee5cecd3bce759d552a68
Andrey Krahmal <coolrighthere@yahoo.com>
2014-01-06 17:14:20 +0000

    Added student count

---
+++ directory.rb
@@ -0,0 +1,27 @@
+#! #!/usr/bin/env ruby
+
+puts '___________________________________________'
+puts 'The students of my cohort at Makers Academy'
+puts '___________________________________________'
+puts 'Andrey'
+puts 'James'
+puts 'Peter'
+puts 'Berta'
+puts 'Megan'
+puts 'Jorja'
+puts 'Nabin'
+puts 'Biwek'
+puts 'Colin'
+puts 'Abraham'
+puts 'Ayaz'
+puts 'Rolando'
+puts 'Kalle'
+puts 'Roy'
+puts 'Liviu'
+puts 'Makis'
+puts '___________________________________________'
+print "Overall we have "
+print 9 
+puts ' great students'
+puts '___________________________________________'
+

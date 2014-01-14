a816e4afc0f1d4070dcf7aee608e59b521c4088b
Andrey Krahmal <coolrighthere@yahoo.com>
2014-01-14 09:25:28 +0000

    Added interactive menu (not yet called, and added some global constants.)

--- directory.rb
+++ directory.rb
@@ -2,38 +2,52 @@
 
 require 'date'
 
-#create an array of students
 
-=begin students_old = [
- { :name =>'Andrey', 	:cohort => :January},
- { :name =>'James', 	:cohort => :January},
- { :name =>'Peter', 	:cohort => :January},
- { :name =>'Berta', 	:cohort => :January},
- { :name =>'Megan', 	:cohort => :January},
- { :name =>'Jorja', 	:cohort => :January},
- { :name =>'Nabin', 	:cohort => :January},
- { :name =>'Biwek', 	:cohort => :January},
- { :name =>'Colin', 	:cohort => :January},
- { :name =>'Abraham', 	:cohort => :January},
- { :name =>'Ayaz', 		:cohort => :January},
- { :name =>'Rolando', 	:cohort => :January},
- { :name =>'Kalle', 	:cohort => :January},
- { :name =>'Roy', 		:cohort => :January},
- { :name =>'Liviu', 	:cohort => :January},
- { :name =>'Makis', 	:cohort => :January}
-]
-=end
+def interactive_menu
+	
+	students = []
 
-#ask for user inout
-def input_students (default_cohort)
-	puts "Please enter the names of the students and their cohorts (default is #{default_cohort}).\nTo finish, just hit return two times."
+	loop do 
+		# - print the menu and ask the user what to do
+		puts '___________________________________________'
+		puts "1. Input the students."
+		puts "2. Show the students."
+		puts "9. Exit."
+		puts '___________________________________________'
+
+		# - read the input and save it into a variable
+		selection = gets.chomp
+
+		# - do what the user has asked
+
+		case selection
+		when "1" # input students
+			students = input_students
+		when "2" # show students
+			print_header
+			print_counter = print_by_cohort(students)
+			print_footer(students, print_counter)
+		when "9" 
+			exit
+		else 
+			puts "I do not know what you mean. Try again."
+		end 
+
+	end
+
+end
+
+
+#ask for user input
+def input_students 
+	puts "Please enter the names of the students and their cohorts (default is #{@default_cohort}).\nTo finish, just hit return two times."
 	#create an empty array
 	students = []
 	#get the first name
 	print "Student name? "
 	name = new_chomp(gets)
 	print "Student cohort month? (Usage: January or Jan, etc.)? " 
-	cohort = validate(gets.chomp, default_cohort)
+	cohort = validate(gets.chomp)
 
 	#while name and cohort are both not empty repeat
 	while !name.empty? do
@@ -44,7 +58,7 @@ def input_students (default_cohort)
 		print "Student name? "
 		name = gets.chomp
 		print "Student cohort? "
-		cohort = validate(gets.chomp, default_cohort)
+		cohort = validate(gets.chomp)
 	end
 	# sort the hash
 	students.sort_by!{|hash| hash[:name]  }
@@ -52,8 +66,8 @@ def input_students (default_cohort)
 	students
 end
 
-def validate(cohort, default_cohort)
-	cohort = default_cohort if cohort.empty?
+def validate(cohort)
+	cohort = @default_cohort if cohort.empty?
 	cohort.capitalize!
 	while !Date::ABBR_MONTHNAMES.include? cohort and !Date::MONTHNAMES.include? cohort do
 		print "Your month does not appear to be valid. Usage: January or Jan, etc. Please reenter: "
@@ -65,44 +79,44 @@ def validate(cohort, default_cohort)
 end
 
 # print the header
-def print_header(no_chars)
+def print_header
 	puts '___________________________________________'
 	puts 'The students of my cohort at Makers Academy'
-	puts 'No  '+ "Name".ljust(no_chars+2)+'Cohort'.center(no_chars+17)
+	puts 'No  '+ "Name".ljust(@no_chars+2)+'Cohort'.center(@no_chars+17)
 	puts '___________________________________________'
 end
 
 # print the body of the table with student names
-def print_only_with_conds(students, no_chars)
+def print_only_with_conds(students)
 	print_counter = 1
 	array_counter = 0
 	while array_counter < students.length do
-		if students[array_counter][:name].length <= no_chars #check the student name is shorter than no_chars
-				puts "#{print_counter}.  " + "#{students[array_counter][:name]}".ljust(no_chars+2, '.') + "#{students[array_counter][:cohort]}".rjust(no_chars+6, '.') 
+		if students[array_counter][:name].length <= @no_chars #check the student name is shorter than @no_chars
+				puts "#{print_counter}.  " + "#{students[array_counter][:name]}".ljust(@no_chars+2, '.') + "#{students[array_counter][:cohort]}".rjust(@no_chars+6, '.') 
 				print_counter+=1
 		end	
 		array_counter+=1			
 	end
 	puts '___________________________________________' unless students.empty?
-	print_counter-1
+	print_counter-=1
 end
 
 # print the footer
-def print_footer(students, number_with_conds, no_chars)
+def print_footer(students, print_counter)
 	puts "Overall we have #{students.length} great student" + (students.length == 1 ? "" : "s" )
 	puts '___________________________________________'
-	puts "We have #{number_with_conds} student" + (number_with_conds == 1 ? "" : "s") + " whose name is fewer than\n#{no_chars} characters."
+	puts "We have #{print_counter} student" + (print_counter == 1 ? "" : "s") + " whose name is fewer than\n#{@no_chars} characters."
 	puts '___________________________________________'
 end
 
-def print_by_cohort(students, no_chars)
+def print_by_cohort(students)
         print_counter = 0;
         Date::MONTHNAMES.each do |month|
                 if !month.nil?
-                        print_counter+=print_only_with_conds(students.select{|entry| entry[:cohort] == month}, no_chars)
+                        print_counter+=print_only_with_conds(students.select{|entry| entry[:cohort] == month})
                 end
-        end 
-    print_counter
+        end
+    return print_counter
 end
 
 def new_chomp(input)
@@ -113,21 +127,18 @@ def new_chomp(input)
 end
 
 
-no_chars = 12
-default_cohort = "January"
+@no_chars = 12
+@default_cohort = "January"
 print_counter = 0
 
 # call the methods
-students = input_students(default_cohort)
+students = input_students
 
 
-#print_header
-#print(students_old)
-#print_footer(students_old)
 
-print_header(no_chars)
-print_counter = print_by_cohort(students, no_chars)
-print_footer(students, print_counter, no_chars)
+print_header
+print_counter = print_by_cohort(students)
+print_footer(students, print_counter)
 
 
         

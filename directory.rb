b76c948c55326d40428aed2d60f47eeaf4770548
Andrey Krahmal <coolrighthere@yahoo.com>
2014-01-14 16:38:48 +0000

    Load students from file: students.csv.

--- directory.rb
+++ directory.rb
@@ -96,6 +96,7 @@ def print_menu
 	puts "1. Input the students."
 	puts "2. Show the students."
 	puts "3. Save the list to students.csv."
+	puts "4. Load the list from students.csv."
 	puts "9. Exit."
 	puts '___________________________________________'
 end
@@ -114,6 +115,8 @@ def process(selection)
 		show_students
 	when "3"
 		save_students
+	when "4"
+		load_students
 	when "9" 
 		exit
 	else 
@@ -131,6 +134,16 @@ def save_students
 	end 
 	file.close
 end
+
+def load_students
+	file = File.open("students.csv", "r")
+	file.readlines.each do |line|
+		name, cohort = line.chomp.split(",")
+		@students << {:name => name, :cohort=> cohort}
+	end
+	file.close
+end
+
 
 @no_chars = 12
 @default_cohort = "January"
